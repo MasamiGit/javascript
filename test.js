4e59378a7ce93520922524e335884aa86dcae56f
MasamiGit <info@masami.main.jp>
2018-07-08 04:35:27 +0900

    0708commit

---
+++ test.js
@@ -0,0 +1,17 @@
+// function hoge()
+// {
+// 	alert("Hello");
+// }
+// var e = document.getElementsByClassName('hoge');
+// e[0].addEventListener("click",hoge,false);
+function test1()
+{
+	var result = test2(20,40);
+	document.write(result);
+}
+function test2(x,y)
+{
+	var result = x + y;
+	return result;
+}
+
